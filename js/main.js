a34efb9b34b8aab51d3719cab0b07292c4b59093
Stan <lepikov.free@yandex.ru>
2015-02-18 22:54:25 +0300

    l

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 (function($){
 
-	
-    
 	$(function() {
+        $('.aside-menu .text').on('click', function(e) {
+            $('.aside-menu .expanded').removeClass('expanded');
+            $(e.target).closest('li').addClass('expanded')
+        });
 	});
 
 })(jQuery,undefined)
